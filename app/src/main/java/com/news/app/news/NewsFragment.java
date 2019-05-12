a0a3678a6b146003457728ace0acc8f709130fcd
liran cohen <lirancr@gmail.com>
2019-05-12 22:13:51 +0300

    no message

--- app/src/main/java/com/news/app/news/NewsFragment.java
+++ app/src/main/java/com/news/app/news/NewsFragment.java
@@ -1,6 +1,26 @@
 package com.news.app.news;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class NewsFragment extends Fragment {
+
+    RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.news_list_frag, container, false);
+        this.recyclerView = v.findViewById(R.id.recycler);
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(v.getContext(), LinearLayoutManager.HORIZONTAL, false));
+        this.recyclerView.setAdapter(new NewsRecyclerAdapter());
+        return v;
+    }
 }
